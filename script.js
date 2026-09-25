152d3bfa068011d5bb35f3985c783fe842f1e404
hamirifar1380-dev <h.amirifar1380@gmail.com>
2026-09-25 19:29:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,104 +1,77 @@
-const faButton = document.querySelector(".language button:nth-child(2)");
-const enButton = document.querySelector(".language button:nth-child(1)");
-
-const translations = {
-
-    en: {
-
-        about: "About",
-        experience: "Experience",
-        noya: "Noya",
-        contact: "Contact"
-
-    },
+const faButton = document.getElementById("fa-btn");
+const enButton = document.getElementById("en-btn");
 
 
-    fa: {
-
-        about: "درباره من",
-        experience: "تجربه کاری",
-        noya: "برند نویا",
-        contact: "ارتباط با من"
-
-    }
-
-};
+function changeLanguage(language) {
 
 
-
-function changeLanguage(lang){
-
-
-    document.documentElement.lang = lang;
+    const elements = document.querySelectorAll("[data-en]");
 
 
-    if(lang === "fa"){
+    elements.forEach(function(element){
 
+
+        if(language === "fa"){
+
+            element.innerText = element.getAttribute("data-fa");
+
+        }
+
+
+        else {
+
+            element.innerText = element.getAttribute("data-en");
+
+        }
+
+
+    });
+
+
+
+    if(language === "fa"){
+
+
+        document.documentElement.lang = "fa";
 
         document.body.style.direction = "rtl";
-
 
         document.body.style.textAlign = "right";
 
 
-        document.querySelector("nav a:nth-child(1)").innerText =
-        translations.fa.about;
+    }
 
 
-        document.querySelector("nav a:nth-child(2)").innerText =
-        translations.fa.experience;
+    else {
 
 
-        document.querySelector("nav a:nth-child(3)").innerText =
-        translations.fa.noya;
-
-
-        document.querySelector("nav a:nth-child(4)").innerText =
-        translations.fa.contact;
-
-
-
-    } else {
-
+        document.documentElement.lang = "en";
 
         document.body.style.direction = "ltr";
-
 
         document.body.style.textAlign = "left";
 
 
-        document.querySelector("nav a:nth-child(1)").innerText =
-        translations.en.about;
-
-
-        document.querySelector("nav a:nth-child(2)").innerText =
-        translations.en.experience;
-
-
-        document.querySelector("nav a:nth-child(3)").innerText =
-        translations.en.noya;
-
-
-        document.querySelector("nav a:nth-child(4)").innerText =
-        translations.en.contact;
-
-
     }
+
 
 
 }
 
 
 
-faButton.addEventListener("click", ()=>{
 
-changeLanguage("fa");
+faButton.addEventListener("click", function(){
+
+    changeLanguage("fa");
 
 });
 
 
-enButton.addEventListener("click", ()=>{
 
-changeLanguage("en");
+
+enButton.addEventListener("click", function(){
+
+    changeLanguage("en");
 
 });
